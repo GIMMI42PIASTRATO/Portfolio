9b5b0bfc155ca9074fc6ab5e26fc05c5d3937c53
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-01-13 17:43:02 +0100

    Refactor: Extracted Hero.jsx and hero.module.css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,30 +1,10 @@
 import MainWrapper from "../../components/mainWrapper/MainWrapper";
-import style from "./home.module.css";
+import Hero from "../../components/hero/Hero";
 
 export default function home() {
     return (
         <MainWrapper>
-            <div className={style.hero_container}>
-                <div className={style.intro_container}>
-                    <p className={`${style.text} ${style.text1}`}>
-                        Hello! I&apos;m <span>Vittorio Bussano</span>.
-                    </p>
-                    <p className={`${style.text} ${style.text2}`}>
-                        I am an aspiring software engineer and full stack web
-                        developer.
-                    </p>
-                    <p className={`${style.text} ${style.text3}`}>
-                        Passionate about tech blogging:{" "}
-                        <span>
-                            I love sharing insights and experiences through
-                            engaging articles in the tech realm.
-                        </span>
-                    </p>
-                </div>
-                <div className={style.img_container}>
-                    <img src="mcpfp - GIMMI42PIASTRATO.png" alt="Logo" />
-                </div>
-            </div>
+            <Hero />
         </MainWrapper>
     );
 }
