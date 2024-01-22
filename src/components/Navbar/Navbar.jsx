4eae757a1b02bde863ee4fafb04c782e2e5b1f36
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-01-22 18:59:19 +0100

    Add framer-motion dependency and update Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,52 @@
+import { useState } from "react";
 import style from "./navbar.module.css";
-import { TbMoon } from "react-icons/tb";
+import { TbMoon, TbMenu, TbX } from "react-icons/tb";
+import { motion } from "framer-motion";
 
 export default function Navbar() {
-    return (
-        <nav className={style.nav_container}>
-            <div className={style.name_container}>
-                <div className={style.img_container}>
-                    <img src="mcpfp - GIMMI42PIASTRATO.png" alt="Logo" />
-                </div>
-                <a href="#home">Vittorio Bussano</a>
-            </div>
-            <span className={style.link_container}>
-                <ul className={style.link}>
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">Blog</a>
-                    </li>
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                </ul>
-                <div className={style.button_container}>
-                    <button className={style.theme_button}>
-                        <TbMoon />
-                    </button>
-                </div>
-            </span>
-        </nav>
-    );
+	const [menu, setMenu] = useState(false);
+
+	const menuButtonVariants = {
+		open: { rotate: 0 },
+		closed: { rotate: 180 },
+	};
+
+	return (
+		<nav className={style.nav_container}>
+			<div className={style.name_container}>
+				<div className={style.img_container}>
+					<img src="mcpfp - GIMMI42PIASTRATO.png" alt="Logo" />
+				</div>
+				<a href="#home">Vittorio Bussano</a>
+			</div>
+			<span className={style.link_container}>
+				<ul className={style.link}>
+					<li>
+						<a href="#">Home</a>
+					</li>
+					<li>
+						<a href="#">Blog</a>
+					</li>
+					<li>
+						<a href="#">About</a>
+					</li>
+				</ul>
+				<div className={style.button_container}>
+					<button className={style.theme_button}>
+						<TbMoon />
+					</button>
+
+					<motion.button
+						className={`${style.theme_button} ${style.menu}`}
+						onClick={() => setMenu(!menu)}
+						variants={menuButtonVariants}
+						initial={false}
+						animate={menu ? "open" : "closed"}
+					>
+						{menu ? <TbX /> : <TbMenu />}
+					</motion.button>
+				</div>
+			</span>
+		</nav>
+	);
 }
