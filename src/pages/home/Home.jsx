56483a264b3b7d36f1a5f1280ecb4b836afa0b9c
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-02-24 14:43:42 +0100

    Refactor created CardsContainer component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,62 +1,13 @@
-// Style import
-import style from "./home.module.css";
-
 // Components imports
 import MainWrapper from "../../components/mainWrapper/MainWrapper";
 import Hero from "../../components/hero/Hero";
-import Subtitle from "../../components/subtitle/Subtitle";
-import Card from "../../components/card/Card";
-
-// Icons import
-import {
-	TbCode,
-	TbServer2,
-	TbBrandPython,
-	TbDeviceMobileCode,
-	TbMathFunction,
-	TbBrandMysql,
-} from "react-icons/tb";
+import CardsContainer from "../../components/cardsContainer/CardsContainer";
 
 export default function home() {
 	return (
 		<MainWrapper>
 			<Hero />
-			<div className={style.main}>
-				<div>
-					<Subtitle text="What i'm great at" />
-					<div className={style.cards_container}>
-						<Card icon={<TbCode />} text="Front-end Development" />
-						<Card
-							icon={<TbServer2 />}
-							text="Back-end Development"
-						/>
-						<Card
-							icon={<TbBrandPython />}
-							text="Python development"
-						/>
-					</div>
-				</div>
-				<div>
-					<Subtitle text="What I am learning" />
-					<div className={style.cards_container}>
-						<Card
-							icon={<TbDeviceMobileCode />}
-							text="Mobile development"
-							className="learning"
-						/>
-						<Card
-							icon={<TbMathFunction />}
-							text="Machine learning / Deep learning"
-							className="learning"
-						/>
-						<Card
-							icon={<TbBrandMysql />}
-							text="MySQL"
-							className="learning"
-						/>
-					</div>
-				</div>
-			</div>
+			<CardsContainer />
 		</MainWrapper>
 	);
 }
