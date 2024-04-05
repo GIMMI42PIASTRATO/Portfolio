0297e7d3d0d464e4bf929f7d47bd3232edcfea41
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-04-05 15:37:09 +0200

    feat(page): added About component to Home page

---
+++ src/app/components/About/About.tsx
@@ -0,0 +1,12 @@
+import style from "./About.module.css";
+
+export default function About() {
+	return (
+		<div>
+			<div className={style.section_title}>
+				<h3>ABOUT VITTORIO BUSSANO</h3>
+				<h3>01</h3>
+			</div>
+		</div>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 
 export default function Home() {
 	return (
-		<main className="flex">
-			<Hero />
+		<main className="flex flex-col">
+			<section className="h-screen">
+				<Hero />
+			</section>
+			<section>
+				<About />
+			</section>
 		</main>
 	);
 }
