e0684465f87a85eed8d20dc2bec6e9898fd0171e
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-04-06 16:38:46 +0200

    feat(about): add image to About component

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -1,4 +1,5 @@
 import style from "./About.module.css";
+import Image from "next/image";
 
 export default function About() {
 	return (
@@ -6,6 +7,15 @@ export default function About() {
 			<div className={style.section_title}>
 				<h3>ABOUT VITTORIO BUSSANO</h3>
 				<h3>01</h3>
+			</div>
+
+			<div className={style.text_container}>
+				<div className={style.about}>
+					<div className={style.image_container}>
+						<img src="/keyboard-image.jpeg" alt="keyboard" />
+					</div>
+				</div>
+				<div className={style.details}></div>
 			</div>
 		</div>
 	);
