eb77e7c632b5b7d5773ef6a51aaa9341a01343f2
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-04-07 15:58:23 +0200

    feat(about): created the about me text

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -1,8 +1,9 @@
-import style from "./About.module.css";
+import style from "./about.module.css";
+import { mulish } from "@/utility/font";
 
 export default function About() {
 	return (
-		<div>
+		<>
 			<div className={style.section_title}>
 				<h3>ABOUT VITTORIO BUSSANO</h3>
 				<h3>01</h3>
@@ -10,17 +11,33 @@ export default function About() {
 
 			<div className={style.text_container}>
 				<div className={style.about}>
-					<div className={style.image_container}>
-						<img src="/keyboard-image.jpeg" alt="keyboard" />
+					<img
+						className={style.about_img}
+						src="/keyboard-image.jpeg"
+						alt="keyboard"
+					/>
+					<div className={style.about_text}>
+						<h2>
+							Embracing the digital frontier as a full-stack web
+							developer and software engineer hailing from the
+							vibrant city of Turin, Italy📍
+						</h2>
+
+						<p className={mulish.className}>
+							With a fervent enthusiasm for both front-end and
+							back-end development, I strive to weave seamless
+							user experiences through my mastery of
+							React/Next.js, Node.js, Express.js, and Python.
+						</p>
+
+						<p className={mulish.className}>
+							Expanding my expertise, I&apos;m currently delving
+							into Android application development with Kotlin.
+						</p>
 					</div>
 				</div>
-				<div className={style.details}>
-					<p>
-						Ciao sono una persona che sa scrivere a battitura per
-						fare delle cose
-					</p>
-				</div>
+				<div className={style.details}></div>
 			</div>
-		</div>
+		</>
 	);
 }
