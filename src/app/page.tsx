b540ac8a0e56170c932e4df1a2163c313ff8e853
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-04-10 19:05:49 +0200

    fixed(hero): fixed a responsive bug in the hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import style from "./page.module.css";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 
 export default function Home() {
 	return (
 		<main className="flex flex-col">
-			<section className="h-screen">
+			<section className={style.hero_section}>
 				<Hero />
 			</section>
 			<section>
