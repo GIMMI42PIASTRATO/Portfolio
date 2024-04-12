bd7e777036da12cda5486611c3e7468e3afdcb52
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-04-12 16:03:37 +0200

    feat(project): created BentoGrid component from Aceternity UI

--- src/app/components/Projects/Projects.tsx
+++ src/app/components/Projects/Projects.tsx
@@ -1,11 +1,28 @@
 import style from "./projects.module.css";
 import SectionTitle from "@/components/SectionTitle/SectionTitle";
+import { BentoGrid, BentoGridItem } from "../BentoGrid/bento-grid";
+import { items } from "@/data/bento-grid-items";
 
 export default function Projects() {
 	return (
 		<>
 			<SectionTitle title="MY PROJECTS" num="02" />
-			<div className={style.project_container}></div>
+			<div className={style.project_container}>
+				<BentoGrid>
+					{items.map((item, i) => (
+						<BentoGridItem
+							key={i}
+							title={item.title}
+							description={item.description}
+							header={item.header}
+							icon={item.icon}
+							className={
+								i === 3 || i === 6 ? "md:col-span-2" : ""
+							}
+						/>
+					))}
+				</BentoGrid>
+			</div>
 		</>
 	);
 }
