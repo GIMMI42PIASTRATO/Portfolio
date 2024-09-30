52b337f33eebebe0ded57b594bc51d87e7958bbf
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-09-30 18:48:18 +0200

    feat: updated PointerFollower to interact with the hero

--- src/app/components/pointer-follower.tsx
+++ src/app/components/pointer-follower.tsx
@@ -1,27 +1,58 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { cn } from "@/utility/cn";
+import { useState, useEffect, useRef } from "react";
 import { throttle } from "lodash";
 import Image from "next/image";
+
+import style from "@/app/components/Hero/hero.module.css";
 
 export default function PointerFollower() {
 	const [position, setPosition] = useState({ x: -100, y: -100 });
 	const [isMouseDevice, setIsMouseDevice] = useState(true);
+	const [isCursorOnHero, setIsCursorOnHero] = useState(false);
+	const heroRef = useRef<Element | null>(null);
 
 	useEffect(() => {
 		// Check if the user is on a touch device
 		const isTouchDevice = window.matchMedia("(pointer: coarse)").matches;
 		setIsMouseDevice(!isTouchDevice);
 
+		heroRef.current = document.querySelector(`.${style.hero_name}`);
+
 		if (!isTouchDevice) {
 			const updpatePosition = throttle((e: MouseEvent) => {
 				setPosition({ x: e.clientX, y: e.clientY });
 			}, 20);
 
+			const handleMouseEnter = () => setIsCursorOnHero(true);
+			const handleMouseLeave = () => setIsCursorOnHero(false);
+
 			window.addEventListener("mousemove", updpatePosition);
+
+			if (heroRef.current) {
+				heroRef.current.addEventListener(
+					"mouseenter",
+					handleMouseEnter
+				);
+				heroRef.current.addEventListener(
+					"mouseleave",
+					handleMouseLeave
+				);
+			}
 
 			return () => {
 				window.removeEventListener("mousemove", updpatePosition);
+				if (heroRef.current) {
+					heroRef.current.removeEventListener(
+						"mouseenter",
+						handleMouseEnter
+					);
+					heroRef.current.removeEventListener(
+						"mouseleave",
+						handleMouseLeave
+					);
+				}
 			};
 		}
 	}, []);
@@ -33,21 +64,31 @@ export default function PointerFollower() {
 	return (
 		<div className="fixed inset-0 pointer-events-none z-50">
 			<div
-				className="relative w-[91px] h-[91px] bg-transparent backdrop-blur-sm rounded-full transform -translate-x-1/2 -translate-y-1/2 transition-all duration-[600ms] ease-out overflow-hidden"
+				className={cn(
+					"relative w-[91px] h-[91px] bg-transparent backdrop-blur-sm rounded-full transform -translate-x-1/2 -translate-y-1/2 transition-all duration-[600ms] ease-out overflow-hidden",
+					isCursorOnHero ? "w-[177px] h-[177px]" : "w-[91px] h-[91px]"
+				)}
 				style={{
 					left: `${position.x}px`,
 					top: `${position.y}px`,
 				}}
 				aria-hidden="true"
 			>
-				<div className="flex justify-center items-center w-full h-full ">
+				<div
+					className={cn(
+						"flex justify-center items-center w-full h-full transition-all duration-[600ms] ease-out",
+						isCursorOnHero
+							? "opacity-0 scale-50"
+							: "opacity-100 scale-100"
+					)}
+				>
 					<Image
 						src="scroll-down.svg"
 						alt="SCROLL"
 						height={80}
 						width={80}
 						quality={100}
-						className="animate-spin-slow "
+						className="animate-spin-slow"
 					/>
 				</div>
 			</div>
